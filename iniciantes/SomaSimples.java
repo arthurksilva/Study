07a1ad267bd600d4ccb1f86c0cd14feea80662b1
Arthur Carvalho da Silva <arthursilvainformatica@gmail.com>
2025-09-04 20:42:15 -0300

    iniciando trabalhos

---
+++ iniciantes/SomaSimples.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SomaSimples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Leia os dois valores inteiros
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        // Calcule a soma
+        int SOMA = A + B;
+
+        // Imprima o resultado
+        System.out.println("SOMA = " + SOMA);
+
+        scanner.close();
+    }
+}
